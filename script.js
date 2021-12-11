826fff2984ddd8bc053fa9c26500dbef2e1f09a0
iqbalmnaufal <iqbalmnaufal@gmail.com>
2021-12-12 02:09:03 +0700

    🚀 Add conditional to prevent error input

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const button = document.getElementsByTagName("button")[0];
 button.onclick = function () {
   const phone = document.querySelector("input").value;
+  if (phone.charAt(0) === "0") {
+    console.log(phone);
+    // remove first char to another char
+    const newPhone = phone.substring(1);
+    // add +38 to the beginning of the phone number
+    const newPhone2 = "62" + newPhone;
+    window.open(`https://wa.me/${newPhone2}`);
+    document.querySelector("input").value = "";
+    return;
+  }
   window.open(`https://wa.me/${phone}`);
   document.querySelector("input").value = "";
 };
